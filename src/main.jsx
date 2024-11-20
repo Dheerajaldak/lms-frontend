7f1d17f2b71ee83c7eaf460a53a594bdbfa5aa8f
Dheeraj Aldak <aldakdheeraj@gmail.com>
2024-11-20 23:00:23 +0530

    Redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux"; //  using Redux
-import store from "./store"; // Redux store setup
+import store from "./Redux/store"; // Redux store setup
 
 // Render the application
 ReactDOM.createRoot(document.getElementById("root")).render(
