21526984fd827360f240221a0eef74722196ef27
Dheeraj Aldak <aldakdheeraj@gmail.com>
2024-11-28 10:36:53 +0530

    Cleand up our about us page

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,39 @@
+import review_1 from "../Assets/Images/review_1.png";
+import review_2 from "../Assets/Images/review_2.png";
+import review_3 from "../Assets/Images/review_3.png";
+import onlineimg from "../Assets/Images/sket.jpg";
+import weare from "../Assets/Images/improf.png";
+
+export const celebrities=[
+    {
+      title: "Nelson Malle",
+      description:"Education is the most powerful tool you can use to change the world. ",
+      image: review_1 ,
+      slideNumber:1
+    },
+    {
+      title: "Gimmi Rae",
+      description:"Failure will never overtake me if my determination to succeed is strong enough. ",
+      image: review_2 ,
+      slideNumber:2
+    },
+    {
+      title: "Albert Innea",
+      description:"A person who never made a mistake never tried anything new. ",
+      image: review_3 ,
+      slideNumber:3
+    },
+    {
+      title: "Adrea Joss",
+      description:"We don't get a chance to do that many things, and every one should be really excellent. ",
+      image: onlineimg ,
+      slideNumber:4
+    },
+    {
+      title: "Jhon Deo",
+      description:"Success is a lousy teacher. It seduces smart people into thinking they can’t lose. ",
+      image: weare ,
+      slideNumber:5
+    },
+
+  ]
