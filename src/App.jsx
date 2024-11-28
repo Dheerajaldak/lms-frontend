ae6b339b0cd92661478e117494fa46c6c584651f
Dheeraj Aldak <aldakdheeraj@gmail.com>
2024-11-28 10:09:35 +0530

    Added about us page and fixed sidebar login signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import AboutUs from "./Pages/AboutUs";
 
 function App() {
   return (
     <>
       <Routes>
       <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/about" element={<AboutUs/>}></Route>
+      
       </Routes>
      
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,html}"],
+  content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/line-clamp")
+  ],
 }
 
